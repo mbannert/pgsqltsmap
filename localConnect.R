4a4cd006aec8b9348cc6284620a411422582e223
Matthias Bannert <bannert@kof.ethz.ch>
2013-09-11 18:14:49 +0200

    +local connect example

--- localConnect.R
+++ localConnect.R
@@ -1,13 +1,17 @@
 # packages
 library(RPostgreSQL)
 
-# works only within KOF!
+# read the blog entry on 
+# learnfrominfo.tumblr.com to 
+# figure out how to run and connect
+# a postgreSQL database on your local 
+# virtual box ubuntu
 drv <- dbDriver("PostgreSQL")
 dbname <- "devseries"
 dbuser <- "devbox"
 dbhost <- "127.0.0.1"
 dbport <- 5430
-pw <- "tant00ine"
+pw <- .rs.askForPassword
 
 con <- dbConnect(drv, host=dbhost, port=dbport, dbname=dbname,
                  user=dbuser, password=pw)
