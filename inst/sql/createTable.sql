24d1780ffbbfff74e6c6bc56839121f0ca2f9bae
Matthias Bannert <bannert@kof.ethz.ch>
2013-11-20 19:55:17 +0100

    updated sql

--- inst/sql/createTable.sql
+++ inst/sql/createTable.sql
@@ -13,20 +13,21 @@ ts_labels hstore,
 primary key (ts_key,ts_language),
 foreign key (ts_key) references timeseries_main (ts_key));
 
-CREATE TABLE ts_vintages (ts_key,vintage_title,
-vintage_data)
-ts_key varchar,
-vintage_title varchar,
+CREATE TABLE ts_vintages (ts_key varchar,
+vintage_key varchar,
+vintage_data hstore,
+primary key (ts_key,vintage_key),
+foreign key (ts_key) references timeseries_main (ts_key));
 
 
 CREATE TABLE timeseries_sets (ts_set_key varchar,
 ts_set_label varchar,
 ts_set_description varchar,
-primarykey (ts_set_key)
+primary key (ts_set_key)
 
 CREATE TABLE ts_key_ts_set_key (ts_key varchar,
 ts_set_key)
-primary key (ts_key varchar,ts_set_key)
+primary key (ts_key varchar,ts_set_key varchar)
 foreign key (ts_key) references timseries_main (ts_key)
 foreign key (ts_set_key) references timeseries_sets (ts_set_key)
 
--- localConnect.R
+++ localConnect.R
@@ -1,17 +1,33 @@
 # packages
 library(RPostgreSQL)
 
-# read the blog entry on 
-# learnfrominfo.tumblr.com to 
-# figure out how to run and connect
-# a postgreSQL database on your local 
-# virtual box ubuntu
+# on your localhost
+# set listen_addresses in postgresql.conf to '*'
+# and add the according information right in pg_hba.conf.
+
 drv <- dbDriver("PostgreSQL")
 dbname <- "devseries"
 dbuser <- "devbox"
 dbhost <- "127.0.0.1"
 dbport <- 5430
-pw <- .rs.askForPassword
+
+
+drv <- dbDriver("PostgreSQL")
+dbname <- "time_series"
+dbuser <- "mbannert"
+dbhost <- "127.0.0.1"
+dbport <- 5430
+
+
 
 con <- dbConnect(drv, host=dbhost, port=dbport, dbname=dbname,
-                 user=dbuser, password=pw)
+                 user=dbuser,
+                 password="mbannert")
+
+
+
+
+
+
+
+# .rs.askForPassword("enter pass for remote db")
