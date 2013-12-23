adf2e4c8d94360d32cafd6d1f0277d2a5dd645c7
Matthias Bannert <bannert@kof.ethz.ch>
2013-12-24 00:57:14 +0100

    time series data.table write to db routine

---
+++ R/ts_data_table_2db.R
@@ -0,0 +1,30 @@
+#' Write a data.table with hstore column to database
+#' 
+#' This function stores a timeseries data.table which contains 
+#' timeseries represented as hstore to a postgres SQL database.
+#' 
+#' @param con database connection object
+#' @param dt a data.table created by \code{\link{env2data.table}}
+#' @param tab name of the database table
+#' @parm overwrite boolean remove existing rows of the same key in the database,
+#' defaults to FALSE
+#' @seealso \code{\link{env2data.table}}
+#' @author Matthias Bannert
+#' @export
+ts_data_table_2db <- function(con,dt,tab="timeseries_main",
+                              overwrite = F){
+  # DELETE all rows from database table tab
+  # where key is IN ts_key of dt
+  if(overwrite){
+    dbGetQuery(conn=con,
+               paste("DELETE FROM",tab,
+                     "WHERE ts_key IN (",
+                     paste(wrap(dt$ts_key,"'"),collapse=","),
+                     ")")
+    )
+  }
+  # write the entire table to datatable
+  dbWriteTable(conn=con,tab,dt,
+               row.names=F,append=T)  
+  
+}
--- test_env2db.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-# assume we got an environment full of time series that needs to be written to 
-load("data/time_series.RData")
-
-
-
-env2data.table("time_series_export")
-
